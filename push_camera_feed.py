72844ba79b53783b1a2ec3f90d3bc9783d4b325b
Status <kuobenj@gmail.com>
2017-11-11 02:26:39 +0000

    made some changes to camera feed and now it works

--- push_camera_feed.py
+++ push_camera_feed.py
@@ -11,14 +11,17 @@
 # print 'photo_pushed'
 
 import sys
+import time
 import pygame
 import pygame.camera
+import pygame.image
+from subprocess import call
 
 pygame.init()
 pygame.camera.init()
 
 #create fullscreen display 640x480
-screen = pygame.display.set_mode((640,480),0)
+#screen = pygame.display.set_mode((640,480),0)
 
 #find, open and start low-res camera
 cam_list = pygame.camera.list_cameras()
@@ -29,14 +32,20 @@ while True:
     #grab image, scale and blit to screen
     imagen = webcam.get_image()
     imagen = pygame.transform.scale(imagen,(640,480))
-    screen.blit(imagen,(0,0))
+ #   screen.blit(imagen,(0,0))
 
     #draw all updates to display
-    pygame.display.update()
 
+  #  pygame.display.update()
+    pygame.image.save(imagen, 'photo.jpg')
+    print 'photo saved'
+#    print call(['whoami'])
+#    print call(['cp', 'photo.jpg', '/var/www/html/photo.jpg'])
+    call(['cp', 'photo.jpg', '/var/www/html/photo.jpg'])
+    time.sleep(1)
     # check for quit events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-        webcam.stop()
-        pygame.quit()
-        sys.exit()
+	        webcam.stop()
+        	pygame.quit()
+	        sys.exit()

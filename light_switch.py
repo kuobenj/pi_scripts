07f0624728a259055acc22d4c8a8699746ff051a
Status Kuo <kuobenj@gmail.com>
2019-05-06 21:06:48 -0700

    fixing script? part2

--- light_switch.py
+++ light_switch.py
@@ -6,26 +6,27 @@ import os
 # Pin Definitons:
 switchPin    = 7
 
-
-def lights_switch_change(switchPin):
-    if not GPIO.input(switchPin):
-        print 'Lights On'
-        os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_on/with/key/b1SgHiGpXqazCsnAuVwLi')
-    else:
-        print 'Lights Off'
-        os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_off/with/key/b1SgHiGpXqazCsnAuVwLi')
-
 # Initial Pin Setup:
 RPi.GPIO.setmode(RPi.GPIO.BOARD) # Broadcom pin-numbering scheme
 
 # Switch Setup:
 RPi.GPIO.setup(switchPin, RPi.GPIO.IN, pull_up_down = RPi.GPIO.PUD_UP)
-RPi.GPIO.add_event_detect(switchPin, RPi.GPIO.BOTH, callback=lights_switch_change, bouncetime=200)  # add falling edge detection on a channel
 
 # Main loop is only event drivent for this script
 print("Here we go! Press CTRL+C to exit")
 try:
+    switch_state = RPi.GPIO.input(switchPin)
     while 1:
-        pass
+        read_val = RPi.GPIO.input(switchPin)
+        if read_val == switch_state:
+            pass
+        else:
+            switch_state = read_val
+            if switch_state:
+                print 'Lights On'
+                os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_on/with/key/b1SgHiGpXqazCsnAuVwLi')
+            else:
+                print 'Lights Off'
+                os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_off/with/key/b1SgHiGpXqazCsnAuVwLi')
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     RPi.GPIO.cleanup() # cleanup all RPi.GPIO

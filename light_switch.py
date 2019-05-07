c86b3b56752391f1db007b2bee4137272775850b
Status Kuo <kuobenj@gmail.com>
2019-05-06 20:56:03 -0700

    fixing script?

--- light_switch.py
+++ light_switch.py
@@ -7,21 +7,20 @@ import os
 switchPin    = 7
 
 
-def lights_on(switchPin):
-    print 'Lights On'
-    os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_on/with/key/b1SgHiGpXqazCsnAuVwLi')
-
-def lights_off(switchPin):
-    print 'Lights Off'
-    os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_off/with/key/b1SgHiGpXqazCsnAuVwLi')
+def lights_switch_change(switchPin):
+    if not GPIO.input(switchPin):
+        print 'Lights On'
+        os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_on/with/key/b1SgHiGpXqazCsnAuVwLi')
+    else:
+        print 'Lights Off'
+        os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_off/with/key/b1SgHiGpXqazCsnAuVwLi')
 
 # Initial Pin Setup:
 RPi.GPIO.setmode(RPi.GPIO.BOARD) # Broadcom pin-numbering scheme
 
 # Switch Setup:
 RPi.GPIO.setup(switchPin, RPi.GPIO.IN, pull_up_down = RPi.GPIO.PUD_UP)
-RPi.GPIO.add_event_detect(switchPin, RPi.GPIO.FALLING, callback=lights_on, bouncetime=200)  # add falling edge detection on a channel
-RPi.GPIO.add_event_detect(switchPin, RPi.GPIO.RISING, callback=lights_off, bouncetime=200)  # add falling edge detection on a channel
+RPi.GPIO.add_event_detect(switchPin, RPi.GPIO.BOTH, callback=lights_switch_change, bouncetime=200)  # add falling edge detection on a channel
 
 # Main loop is only event drivent for this script
 print("Here we go! Press CTRL+C to exit")

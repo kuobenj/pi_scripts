4e9f1b2cf1659f323d95a599b8408eab8a4cef09
Status Kuo <kuobenj@gmail.com>
2020-03-27 20:42:54 -0700

    Adding Updates for LED Strip

--- light_switch.py
+++ light_switch.py
@@ -5,30 +5,84 @@ import time
 #can put this in /etc/rc.local
 
 # Pin Definitons:
-switchPin    = 7
+switchPin1    = 7
+switchPin2    = 32
+
+LEDPin1 = 33
+LEDPin2 = 35
+LEDPin3 = 37
 
 # Initial Pin Setup:
 RPi.GPIO.setmode(RPi.GPIO.BOARD) # Broadcom pin-numbering scheme
 
 # Switch Setup:
-RPi.GPIO.setup(switchPin, RPi.GPIO.IN, pull_up_down = RPi.GPIO.PUD_UP)
+RPi.GPIO.setup(switchPin1, RPi.GPIO.IN, pull_up_down = RPi.GPIO.PUD_UP)
+RPi.GPIO.setup(switchPin2, RPi.GPIO.IN, pull_up_down = RPi.GPIO.PUD_UP)
+
+RPi.GPIO.setup(LEDPin1, RPi.GPIO.OUT)
+RPi.GPIO.setup(LEDPin2, RPi.GPIO.OUT)
+RPi.GPIO.setup(LEDPin3, RPi.GPIO.OUT)
 
 # Main loop is only event drivent for this script
 print("Here we go! Press CTRL+C to exit")
 try:
-    switch_state = RPi.GPIO.input(switchPin)
+    LED_val = 0
+    switch1_state = RPi.GPIO.input(switchPin1)
     while 1:
-        read_val = RPi.GPIO.input(switchPin)
-        if read_val == switch_state:
+        read_val1 = RPi.GPIO.input(switchPin1)
+        if read_val1 == switch1_state:
             pass
         else:
-            switch_state = read_val
-            if switch_state:
+            switch1_state = read_val1
+            if switch1_state:
                 print '\nLights Off\n'
                 os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_off/with/key/b1SgHiGpXqazCsnAuVwLi')
             else:
                 print '\nLights On\n'
                 os.system('curl -X POST https://maker.ifttt.com/trigger/bed_lamp_on/with/key/b1SgHiGpXqazCsnAuVwLi')
+        switch2_state = RPi.GPIO.input(switchPin2)
+        if switch2_state:
+            pass
+        else:
+            LED_val = LED_val + 1
+            LED_val = LED_val % 8
+            if LED_val == 0:
+                RPi.GPIO.output(LEDPin1, False)
+                RPi.GPIO.output(LEDPin2, False)
+                RPi.GPIO.output(LEDPin3, False)
+            elif LED_val == 1:
+                RPi.GPIO.output(LEDPin1, True)
+                RPi.GPIO.output(LEDPin2, True)
+                RPi.GPIO.output(LEDPin3, True)
+            elif LED_val == 2:
+                RPi.GPIO.output(LEDPin1, True)
+                RPi.GPIO.output(LEDPin2, False)
+                RPi.GPIO.output(LEDPin3, False)
+            elif LED_val == 3:
+                RPi.GPIO.output(LEDPin1, False)
+                RPi.GPIO.output(LEDPin2, True)
+                RPi.GPIO.output(LEDPin3, False)
+            elif LED_val == 4:
+                RPi.GPIO.output(LEDPin1, False)
+                RPi.GPIO.output(LEDPin2, False)
+                RPi.GPIO.output(LEDPin3, True)
+            elif LED_val == 5:
+                RPi.GPIO.output(LEDPin1, True)
+                RPi.GPIO.output(LEDPin2, True)
+                RPi.GPIO.output(LEDPin3, False)
+            elif LED_val == 6:
+                RPi.GPIO.output(LEDPin1, False)
+                RPi.GPIO.output(LEDPin2, True)
+                RPi.GPIO.output(LEDPin3, True)
+            elif LED_val == 7:
+                RPi.GPIO.output(LEDPin1, True)
+                RPi.GPIO.output(LEDPin2, False)
+                RPi.GPIO.output(LEDPin3, True)
+            else:
+                RPi.GPIO.output(LEDPin1, False)
+                RPi.GPIO.output(LEDPin2, False)
+                RPi.GPIO.output(LEDPin3, False)
+
         time.sleep(1)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     RPi.GPIO.cleanup() # cleanup all RPi.GPIO
